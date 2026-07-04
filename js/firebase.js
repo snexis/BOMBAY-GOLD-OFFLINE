4362e8fe4bc38f04e49aa7e8e1150ca48da01847
landsolutionin <sonu26bolg@gmail.com>
2026-07-05 02:24:38 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,28 +1,34 @@
-// Firebase Configuration
-
+// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-  getDatabase,
-  ref,
-  set,
-  push,
-  update,
-  remove,
-  get,
-  onValue
+getDatabase,
+ref,
+set,
+get,
+push,
+remove,
+update,
+onValue
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Your Firebase Config
-
+// Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDS5w8TPFVLoy1i-RFJlRaCY_8LG9Xmpuw",
-  authDomain: "liveresultboard.firebaseapp.com",
-  databaseURL: "https://liveresultboard-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "liveresultboard",
-  storageBucket: "liveresultboard.firebasestorage.app",
-  messagingSenderId: "464276956164",
-  appId: "1:464276956164:web:68a0c1b7648e8d9f45c151"
+
+apiKey: "AIzaSyDS5w8TPFVLoy1i-RFJlRaCY_8LG9Xmpuw",
+
+authDomain: "liveresultboard.firebaseapp.com",
+
+databaseURL: "https://liveresultboard-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+projectId: "liveresultboard",
+
+storageBucket: "liveresultboard.firebasestorage.app",
+
+messagingSenderId: "464276956164",
+
+appId: "1:464276956164:web:68a0c1b7648e8d9f45c151"
+
 };
 
 // Initialize Firebase
@@ -31,15 +37,24 @@ const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
 
-// Export Everything
+// Export
 
 export {
-  db,
-  ref,
-  set,
-  push,
-  update,
-  remove,
-  get,
-  onValue
+
+db,
+
+ref,
+
+set,
+
+get,
+
+push,
+
+remove,
+
+update,
+
+onValue
+
 };
